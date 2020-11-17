e84c169aa0dfbf7578ff4443513ef5a0651ed0de
KamiloBarrera <kamilo_252@hotmail.com>
2020-11-17 12:00:23 -0300

    feat(Project): New Interface -> Contracts

---
+++ app/src/main/java/cl/ucn/disc/dsm/cbarrera/mynews/services/Contracts.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright(c)
+ *
+ * Copyright 2020 Camilo Barrera Arancibia, camilo.barrera@alumnos.ucn.cl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package cl.ucn.disc.dsm.cbarrera.mynews.services;
+
+import java.util.List;
+
+import cl.ucn.disc.dsm.cbarrera.mynews.model.News;
+
+/**
+ * The Contracts from CU/UH.
+ *
+ * @author Camilo Barrera A., camilo.barrera@alumnos.ucn.cl
+ */
+public interface Contracts {
+    /**
+     * Get List of news.
+     * @param size size of the list.
+     * @return List of the news.
+     */
+    List<News> retrieveNews(Integer size);
+}
