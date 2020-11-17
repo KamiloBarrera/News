f53040b3ec9d5a8ba9cb4c6d870b0752cc0cd68c
KamiloBarrera <kamilo_252@hotmail.com>
2020-11-17 13:07:31 -0300

    test(Project): Testing ContractsImpl Class and Contracts Interface

---
+++ app/src/test/java/cl/ucn/disc/dsm/cbarrera/mynews/services/TestContractsImpl.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright(c)
+ *
+ * Copyright 2020 Camilo Barrera Arancibia, camilo.barrera@alumnos.ucn.cl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package cl.ucn.disc.dsm.cbarrera.mynews.services;
+
+import com.github.javafaker.Faker;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+
+import java.util.List;
+
+import cl.ucn.disc.dsm.cbarrera.mynews.model.News;
+
+/**
+ * Test of Contract Interface.
+ *
+ * @author Camilo Barrera A., camilo.barrera@alumnos.ucn.cl
+ */
+public class TestContractsImpl {
+    private static final Logger log = LoggerFactory.getLogger(TestContractsImpl.class);
+
+    /**
+     * The test of Retrieve news.
+     */
+    @Test
+    public void testRetrieveNews(){
+        Contracts contracts = new ContractsImpl();
+
+        List<News> news = contracts.retrieveNews(5);
+        Assertions.assertNotNull(news,"List was null:(");
+        Assertions.assertTrue(news.size() != 0,"Empty list? :(");
+        Assertions.assertTrue(news.size() == 5,"List size != 5 :(");
+
+    }
+    /**
+     * Show the faker
+     */
+    @Test
+    public void testFaker(){
+        //Build the faker
+        Faker faker = Faker.instance();
+
+        for(int i = 0;i<5;i++){
+            log.debug("Name: {}",faker.name().fullName());
+            // FIXME: Remover
+            System.out.println("Name: "+faker.name().fullName());
+            System.out.println("Cat: "+faker.cat().name());
+        }
+    }
+}
